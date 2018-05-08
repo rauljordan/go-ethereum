a391017de17fdf570ca3dee695dd90307d6ea5d3
nisdas <nish1993@hotmail.com>
2018-05-08 17:33:03 +0800

    sharding: Removing Test (#92)

--- sharding/collation_test.go
+++ sharding/collation_test.go
@@ -43,40 +43,6 @@ func TestCollation_AddTransactions(t *testing.T) {
 	}
 }
 
-func TestCollation_GasUsed(t *testing.T) {
-	tests := []struct {
-		transactions []*types.Transaction
-		gasUsed      *big.Int
-	}{
-		{
-			transactions: []*types.Transaction{
-				makeTxWithGasLimit(100),
-				makeTxWithGasLimit(100000),
-				makeTxWithGasLimit(899900),
-			},
-			gasUsed: big.NewInt(1000000),
-		}, {
-			transactions: []*types.Transaction{},
-			gasUsed:      big.NewInt(0),
-		},
-		{
-			transactions: []*types.Transaction{
-				makeTxWithGasLimit(math.MaxUint64),
-				makeTxWithGasLimit(9001),
-				makeTxWithGasLimit(math.MaxUint64),
-			},
-			gasUsed: big.NewInt(0).SetUint64(math.MaxUint64),
-		},
-	}
-
-	for _, tt := range tests {
-		got := (&Collation{transactions: tt.transactions}).GasUsed()
-		if tt.gasUsed.Cmp(got) != 0 {
-			t.Errorf("Returned unexpected gasUsed. Got=%v, wanted=%v", got, tt.gasUsed)
-		}
-	}
-}
-
 func TestSerialize(t *testing.T) {
 	tests := []struct {
 		transactions []*types.Transaction
